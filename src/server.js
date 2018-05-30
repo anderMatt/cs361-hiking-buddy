ac7b45c100ff0195a3d50eb2fadb04ff1fcda67e
Colton Herinckx <herinckc@flip3.engr.oregonstate.edu>
2018-05-30 06:39:23 -0700

    added mysql to server.js and added error handling

--- src/server.js
+++ src/server.js
@@ -7,12 +7,14 @@ const express = require('express');
 const handlebars = require('express-handlebars').create({defaultLayout: 'main'});
 const routes = require ('./routes');
 const bodyParser = require('body-parser');
+const mysql = require('./dbcon.js');
 
 // Initial configuration
 var app = express();
 app.engine('handlebars', handlebars.engine);
 app.set('view engine', 'handlebars');
 app.set('port', process.argv[2] || 3000);
+app.set('mysql', mysql);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
@@ -26,10 +28,17 @@ routes.init(app);
 
 /******************************
 Error Handler
-
-Add me!
 ******************************/
+app.use(function(req,res){
+	res.status(404);
+	res.render('404');
+});
 
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500);
+  res.render('500');
+});
 
 
 /******************************
